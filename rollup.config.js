a30a0f29664e962823e0dc65ec98c45491383cf3
Pepijn Verburg <pepijn@moeilijkedingen.nl>
2022-11-29 15:37:46 +0100

    fix: fixed issue where the types were not bundled yet, which could cause confusion when including the package and seeing all sub directories also being available to be imported even though those files don't really exist.

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,13 @@
 import typescript from '@rollup/plugin-typescript';
 import url from '@rollup/plugin-url';
+import dts from 'rollup-plugin-dts';
 
-export default {
+/**
+ * Bundling will go in two major steps:
+ * 1. Transpiling the typescript to ESM and bundling all audio files.
+ * 2. Bundling all type definitions to match the public API of the package.
+ */
+export default [{
   input: 'src/index.ts',
   output: {
     dir: 'dist',
@@ -13,4 +19,11 @@ export default {
       include: ['**/*.ogg', '**/*.mp3', '**/*.aac'],
     }),
   ],
-};
+}, {
+  input: './dist/types/index.d.ts',
+  output: [{
+    file: 'dist/index.d.ts',
+    format: 'es'
+  }],
+  plugins: [dts()],
+}];
