93c61cf780f39a1723764cf995edcaed3a3f9807
Pepijn Verburg <pepijn.verburg@gmail.com>
2023-04-24 17:44:06 +0200

    feat: published all the types to be available when importing the library. This allows users to define their own objects with the correct type definitions if needed.

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,21 @@
+/**
+ * Published hooks
+ */
 export { useClippet } from './hooks/useClippet';
 export { useClippetProvider } from './hooks/useClippetProvider';
 export { useClippetWindowEvents } from './hooks/useClippetWindowEvents';
+
+/**
+ * Published providers
+ */
 export { ClippetProvider } from './context/ClippetProvider';
+
+/**
+ * Published clips
+ */
 export * from './clips';
+
+/**
+ * Published types
+ */
+export * from './types';
