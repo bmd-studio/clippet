36ae1969c5b65028c28d3a69848a66259d3a318b
Pepijn Verburg <pepijn.verburg@gmail.com>
2023-04-25 14:34:58 +0200

    fix: fixed the building and bundling of the `free` sound font and the `react` library to work with the new mono-repo structure.

---
+++ packages/rollup.config.js
@@ -0,0 +1,29 @@
+const typescript = require('@rollup/plugin-typescript').default;
+const url = require('@rollup/plugin-url').default;
+const dts = require('rollup-plugin-dts').default;
+
+/**
+ * Bundling will go in two major steps:
+ * 1. Transpiling the typescript to ESM and bundling all audio files.
+ * 2. Bundling all type definitions to match the public API of the package.
+ */
+module.exports = [{
+  input: 'src/index.ts',
+  output: {
+    dir: 'dist',
+    format: 'es',
+  },
+  plugins: [
+    typescript(),
+    url({
+      include: ['**/*.ogg', '**/*.mp3', '**/*.aac'],
+    }),
+  ],
+}, {
+  input: './dist/types/index.d.ts',
+  output: [{
+    file: 'dist/index.d.ts',
+    format: 'es'
+  }],
+  plugins: [dts()],
+}];
